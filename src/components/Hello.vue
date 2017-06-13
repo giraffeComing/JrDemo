891c6eecc3e24311078999fa7771027efb4b879d
Wei Zhang <zhangwei05@lianjia.com>
2017-06-13 17:59:51 +0800

    引入swiper及mockjs

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="hello">
-    <v-swiper></v-swiper>
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <!--轮播 调用轮播的时候传入相应的轮播配置和数据-->
+    <v-swiper :opts="swiperOpts" :dataVal="imgs"></v-swiper>
+    <!--<h1>{{ msg }}</h1>-->
+    <!--<h2>Essential Links</h2>-->
+    <!--<ul>-->
+      <!--<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>-->
+      <!--<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>-->
+      <!--<li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>-->
+      <!--<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>-->
+      <!--<br>-->
+      <!--<li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>-->
+    <!--</ul>-->
+    <!--<h2>Ecosystem</h2>-->
+    <!--<ul>-->
+      <!--<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>-->
+      <!--<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>-->
+      <!--<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>-->
+      <!--<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>-->
+    <!--</ul>-->
   </div>
 </template>
 
 <script>
-  import VSwiper from "./swiper"
+  import VSwiper from "./swiper";
+  //mock数据
+  import Mock from "mockjs"
 export default {
   name: 'hello',
     components:{
@@ -30,6 +33,30 @@ export default {
     },
   data () {
     return {
+        swiperOpts:{
+            pagination: '.swiper-pagination',
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            slidesPerView: 1,
+            speed:200,
+            paginationClickable: true,
+            loop: true,
+            autoplay: 4000,
+        },
+        imgs:[
+            {
+                "imgSrc":Mock.mock("@image('500x200', '#d9dded', '#FFF', 'jpg', '')"),
+                "href":"http://www.soufeel.com/",
+            },
+            {
+                "imgSrc":Mock.mock("@image('500x200', '#d3e0df', '#FFF', 'jpg', '')"),
+                "href":"http://www.soufeel.com/",
+            },
+            {
+                "imgSrc":Mock.mock("@image('500x200', '#d3cccc', '#FFF', 'jpg', '')"),
+                "href":"http://www.soufeel.com/",
+            }
+        ],
       msg: 'Welcome to Your Vue.js App'
     }
   }
--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,18 +1,117 @@
 <template>
-    <div>ssssss
-        <p>sss</p>
+    <div class="common-swiper">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="item in dataVal"><a :href="item.href"><img :src="item.imgSrc"/></a></div>
+            </div>
+            <!--分页及按钮-->
+            <div class="swiper-pagination"></div>
+            <!--<div class="swiper-button-next"><i class="fa fa-angle-right"></i></div>-->
+            <!--<div class="swiper-button-prev"><i class="fa fa-angle-left"></i></div>-->
+        </div>
     </div>
 </template>
 <script>
+    //引入swiper
+    import swiper from 'swiper';
+    import swipercss from 'swiper/dist/css/swiper.css';
     export default{
-        name: '',
+        name: 'Swiper',
+        props:{
+            //swiper的配置项
+            opts:{},
+            //swiper的数据
+            dataVal:{
+                type:Array
+            },
+        },
         data(){
-            return {}
+            return {
+
+            }
+        },
+        mounted(){
+            //挂在完成之后初始化swiper
+            var commonSwiper = new Swiper('.swiper-container', this.opts);
         }
     }
 </script>
 <style lang="scss" rel="stylesheet/scss">
-    div{
-        p{ color: red}
+    .common-swiper{
+        .swiper-container {
+            width: 100%;
+            height: 200px;
+            .swiper-button-next{
+                width: 50px;
+                height: 50px;
+                border-radius: 25px;
+                background: rgba(0,0,0,0.2);
+                transform: scale(0);
+                transition: all ease-in-out .3s !important;
+                text-align: center;
+                i{
+                    font-size: 28px;
+                    color: #fff;
+                    padding-top: 11px;
+                }
+            }
+            .swiper-button-prev{
+                width: 50px;
+                height: 50px;
+                border-radius: 25px;
+                background: rgba(0,0,0,0.2);
+                transform: scale(0);
+                transition: all ease-in-out .3s !important;
+                text-align: center;
+                i{
+                    font-size: 28px;
+                    color: #fff;
+                    padding-top: 11px;
+                }
+            }
+            /*.swiper-button-next{*/
+                /*&:hover{*/
+                    /*background: rgba(0,0,0,0.8);*/
+                /*}*/
+            /*}*/
+            /*.swiper-button-prev{*/
+                /*&:hover{*/
+                    /*background: rgba(0,0,0,0.8);*/
+                /*}*/
+            /*}*/
+            /*&:hover{*/
+                /*.swiper-button-next{*/
+                    /*transform: scale(1);*/
+                /*}*/
+                /*.swiper-button-prev{*/
+                    /*transform: scale(1);*/
+                /*}*/
+            /*}*/
+        }
+        .swiper-slide {
+            text-align: center;
+            font-size: 18px;
+            background: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            a{ display: block}
+            img{
+                width: 100%;
+                height: 500px;
+            }
+        }
+        .swiper-wrapper{
+            transition-timing-function: ease, step-start, cubic-bezier(0.1, 0.7, 1.0, 0.1);
+        }
+        .swiper-pagination{
+            transition: all .6s ease-in-out !important;
+            &:hover{
+                transform: scale(2);
+            }
+        }
+        .swiper-pagination-bullet-active{
+            background: #333 !important;
+        }
     }
 </style>
