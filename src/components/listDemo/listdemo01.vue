da26bb7d1ef43b5b02af15332413cf4f6a54a5ef
STAFF\zhangwei36 <1004695936@qq.com>
2017-06-20 19:22:49 +0800

    完善百分比圆环组件

--- src/components/listDemo/listdemo01.vue
+++ src/components/listDemo/listdemo01.vue
@@ -5,14 +5,14 @@
                 <div class="wrap-border">
                     <h4>环形进度</h4>
                     <!--环形进度条-->
-                    <div style="float: left" v-for="item in circleProgress.array">
+                    <div class="circlefl" v-for="item in circleProgress.array">
                         <v-circle-progress
                                 :parentValue="item.value"
                                 :radius="item.radius"
                                 :borderWidth="item.borderWidth"
                                 :borderColor="item.borderColor"></v-circle-progress>
                     </div>
-                    <div style="float: left">
+                    <div class="precode">
                         <pre>{{formatCircleProgress}}</pre>
                     </div>
                 </div>
@@ -50,7 +50,7 @@
         data() {
             return {
                 aa:'',
-//                mock数据，使用的时候要注意circleProgress.array才行
+//                mock数据，使用的时候要注意circleProgress.array
                 circleProgress:Mock.mock({
                     "array|6": [
                         {
@@ -69,46 +69,6 @@
                     ]
                 }),
 
-                //svg进度条,radius圆半径，value进度(0~1)，borderWidth边框宽度,borderColor边框颜色
-//                circleProgress:[
-//                    {
-//                        value: 0.8,
-//                        radius: 40,
-//                        borderWidth: 2,
-//                        borderColor: '#77d4ff'
-//                    },
-//                    {
-//                        value: 0.4,
-//                        radius: 40,
-//                        borderWidth: 4,
-//                        borderColor: '#15d755'
-//                    },
-//                    {
-//                        value: 0.8,
-//                        radius: 40,
-//                        borderWidth: 6,
-//                        borderColor: '#ffa54e'
-//                    },
-//                    {
-//                        value: 0.3,
-//                        radius: 60,
-//                        borderWidth: 4,
-//                        borderColor: '#fd4d77'
-//                    },
-//                    {
-//                        value: 0.6,
-//                        radius: 60,
-//                        borderWidth: 6,
-//                        borderColor: '#d56fe8'
-//                    },
-//                    {
-//                        value: 0.9,
-//                        radius: 60,
-//                        borderWidth: 8,
-//                        borderColor: '#898ef7'
-//                    }
-//
-//                ],
                 //步骤条
                 bar: [{
                     instep: true,
@@ -137,6 +97,17 @@
     }
 </script>
 <style lang=scss rel="stylesheet/scss">
+    .circlefl{
+        float: left
+    }
+    .precode{
+        float: right;
+        background: #dce2e9;
+    }
+    pre{
+        font-family: Arial !important;
+        color: #5e5e5e
+    }
     .v-enter {
         opacity: 0;
         transform: translateY(-.5rem);
