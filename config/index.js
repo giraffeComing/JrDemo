6a48c0c6448605b01b93b48ce691654cfc36e626
STAFF\zhangwei36 <1004695936@qq.com>
2017-07-21 17:56:02 +0800

    打包路径修改

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
       //assetsPublicPath这个是index.html中资源路径的配置修改
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
