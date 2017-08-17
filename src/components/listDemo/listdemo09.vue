cb1a5ac275ea7bae3521ae6f64e607a880357971
STAFF\zhangwei36 <1004695936@qq.com>
2017-08-17 17:04:57 +0800

    transition六种过渡状态的问题终于得解啦！

--- src/components/listDemo/listdemo09.vue
+++ src/components/listDemo/listdemo09.vue
@@ -2,7 +2,17 @@
     <div>
         <button @click="showHeart">showbtn</button>
         <button @click="hideHeart">hidebtn</button>
-        <transition name="a">
+        <!--<transition name="a">-->
+            <!--<div class="shape" v-if="ifShow"></div>-->
+        <!--</transition>-->
+        <transition  v-on:before-enter="beforeEnter"
+                     v-on:enter="enter"
+                     v-on:after-enter="afterEnter"
+                     v-on:enter-cancelled="enterCancelled"
+                     v-on:before-leave="beforeLeave"
+                     v-on:leave="leave"
+                     v-on:after-leave="afterLeave"
+                     v-on:leave-cancelled="leaveCancelled">
             <div class="shape" v-if="ifShow"></div>
         </transition>
     </div>
@@ -17,12 +27,69 @@
                 ifShow:false
             }
         },
+        created:function () {
+
+        },
         methods:{
             showHeart:function () {
                 this.ifShow=true
             },
             hideHeart:function () {
                 this.ifShow=false
+            },
+            beforeEnter: function (el) {
+
+//                进入时候的初始状态在beforeEnter中定义
+                el.style.background='red'
+                el.style.transition='all 1s'
+            },
+            // 此回调函数是可选项的设置
+            // 与 CSS 结合时使用
+            enter: function (el, done) {
+
+//                进入时候的过渡状态在enter中定义
+                setTimeout(()=>{
+                    el.style.background='green'
+                },1000)
+                done()
+            },
+            afterEnter: function (el) {
+
+            },
+            enterCancelled: function (el) {
+                // ...
+            },
+            // --------
+            // 离开时
+            // --------
+            beforeLeave: function (el) {
+//                离开时候的初始状态在beforeLeave中定义
+//                注意opacity要设置成1！！！！！！要不立刻就透明了就看不到效果了！！！！
+                el.style.background='blue';
+//                el.style.transition='10s';
+                el.style.opacity='1'
+            },
+            // 此回调函数是可选项的设置
+            // 与 CSS 结合时使用
+            leave: function (el, done) {
+//                离开时候的终止状态在leave中定义
+                setTimeout(()=>{
+                    el.style.opacity='0'
+                    el.style.background='yellow';
+                    el.style.transition='all 4s'
+
+                    setTimeout(()=>{
+                        done()
+                    },4000)
+                },2000)
+
+            },
+            afterLeave: function (el) {
+                // ...
+            },
+            // leaveCancelled 只用于 v-show 中
+            leaveCancelled: function (el) {
+                // ...
             }
         },
     }
@@ -55,7 +122,7 @@
     不能任性地越变，比如translateX也不能越变，*/
     .a-leave{
         opacity: 1;
-        background: red !important;
+        background: blue;
         transform: rotate(0deg);
         /*border: 20px solid black;*/
     }
